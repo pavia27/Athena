abd4abe06278cc34adb4f6d0f0a15b17f69e3e20
pavia27 <pavia27@gmail.com>
2025-09-19 15:48:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Create the environment 
+python3 -m venv research_agent
+
+# Activate it
+source research_agent/bin/activate
+
+# Install dependencies
+python -m pip install -r requirements.txt
---
+++ tools.py
@@ -0,0 +1,23 @@
+from langchain_community.tools import DuckDuckGoSearchRun, ArxivQueryRun
+from langchain_community.tools.pubmed.tool import PubmedQueryRun
+from langchain.tools import Tool
+from datetime import datetime
+
+def save_to_txt(data: str, filename: str = "research_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(formatted_text)
+
+save_tool = Tool(
+    name = "save_text_to_file",
+    func=save_to_txt,
+    description="Save structured research data to a text file"
+)
+
+search = DuckDuckGoSearchRun()
+
+pubmed_tool = PubmedQueryRun()
+
+arxiv_tool = ArxivQueryRun()
