57d64f1adb782348d6ed8397ce9a344c267bda26
pavia27 <pavia27@gmail.com>
2025-09-25 12:09:59 -0700

    updated save function

--- tools.py
+++ tools.py
@@ -5,17 +5,22 @@ from langchain_community.tools import DuckDuckGoSearchRun, ArxivQueryRun
 from langchain_community.tools.pubmed.tool import PubmedQueryRun
 from langchain_community.utilities import PubMedAPIWrapper
 
-def save_to_txt(data: str, filename: str = "research_output.txt"):
+def save_to_txt(data: str, filename: str ):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
-
     with open(filename, "a", encoding="utf-8") as f:
         f.write(formatted_text)
 
-save_tool = Tool(
-    name = "save_text_to_file",
+save_tool = Tool.from_function(
+    name="save_text_to_file",
     func=save_to_txt,
-    description="Save structured research data to a text file"
+    description="Save structured research data to a text file. The input should be a dictionary with 'data' and 'filename' keys."
+)
+
+save_tool = Tool.from_function(
+    name="save_text_to_file",
+    func=save_to_txt,
+    description="Save structured research data to a text file. The input should be a dictionary with 'data' and 'filename' keys."
 )
 
 pubmed_api_wrapper = PubMedAPIWrapper(ncbi_api_key=os.getenv("NCBI_API_KEY"))
@@ -27,7 +32,5 @@ pubmed_tool = Tool(
 )
 
 search_tool = DuckDuckGoSearchRun()
-
-pubmed_tool = PubmedQueryRun()
 
 arxiv_tool = ArxivQueryRun()
